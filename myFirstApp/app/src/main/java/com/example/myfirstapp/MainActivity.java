01215d1bef2f8872001ce419770c89b8a31b88d4
shadaflorida <shada.thykandy@ufl.edu>
2019-01-19 14:51:45 -0500

    second update to main

--- myFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ myFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }*/
 
     public static void main(String [] args)
     {
